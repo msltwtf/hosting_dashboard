dbb109e60cedfef62882e0a6f9099dd55236068a
Marius Leonhardt <info@marius-leonhardt.de>
2022-10-11 01:53:07 +0200

    wip

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/msltwtf/hosting_dashboard
+
+go 1.19
+
+require (
+	github.com/hetznercloud/hcloud-go v1.35.3
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"encoding/json"
+	"errors"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/hetznercloud/hcloud-go/hcloud"
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed templates/*
+
+var content embed.FS
+
+type ServerData struct {
+	Name      string
+	PublicNet hcloud.ServerPublicNet
+	Labels    map[string]string
+}
+
+type NamespaceData struct {
+	Namespace string
+	Servers []ServerData
+}
+
+type toolConfig struct {
+	Namespaces map[string]string
+}
+
+var config toolConfig
+var configfile string
+
+func (c *toolConfig) Load(filename string) error {
+	if strings.HasSuffix(filename, ".json") {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return json.Unmarshal(data, &config)
+	} else if strings.HasSuffix(filename, ".yml") || strings.HasSuffix(filename, ".yaml") {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return yaml.Unmarshal(data, &config)
+	} else {
+		return errors.New("invalid file format! please use json or yaml")
+	}
+}
+
+func getFromAll(w http.ResponseWriter, r *http.Request) {
+	var AllData []NamespaceData
+	for namespace, token := range config.Namespaces {
+		client := hcloud.NewClient(hcloud.WithToken(token))
+		servers, err := client.Server.All(context.Background())
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		var td []ServerData
+		for _, s := range servers {
+			sd := ServerData{
+				Name:      s.Name,
+				PublicNet: s.PublicNet,
+				Labels:    s.Labels,
+			}
+			td = append(td, sd)
+		}
+		data := NamespaceData{
+			Namespace: namespace,
+			Servers: td,
+		}
+		AllData = append(AllData, data)
+	}
+
+
+
+	t, err := template.ParseFS(content, "templates/servers.gohtml")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	d := struct {
+		AllData []NamespaceData
+	}{
+		AllData: AllData,
+	}
+	err = t.Execute(w, d)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func main() {
+	flag.StringVar(&configfile, "c", "config.yml", "Path to config file")
+	flag.Parse()
+	log.Println("Starting Hosting Dashboard")
+	log.Println("Loading Config")
+	if err := config.Load(configfile); err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println("Loaded config")
+
+	http.HandleFunc("/", getFromAll)
+	http.ListenAndServe(":8080", nil)
+}
